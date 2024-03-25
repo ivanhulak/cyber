b42811119d18ca13a1386570142abdc88059a8b1
ivanhulak <g.ivan.gulak17@gmail.com>
2024-03-25 15:28:48 +0200

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,28 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "blue-black": "#090538",
+        "portland-orange": "#FF5635",
+        gainsboro: "D9D9D9",
+      },
+      fontFamily: {
+        georgia: [
+          "Georgia",
+          "ui-serif",
+          "Cambria",
+          "Times New Roman",
+          "Times",
+          "serif",
+        ],
+      },
+      fontSize: {
+        "10xl": ["11.25rem", "1"],
       },
     },
   },
